aef0c5b5ef39a43f647f271dfd9820014e2ff4e7
Kedar Patil <knpatil512@gmail.com>
2020-02-01 18:06:34 +0530

    Added git ignore and some changes

--- src/com/training/GitTraining.java
+++ src/com/training/GitTraining.java
@@ -3,5 +3,6 @@ package com.training;
 public class GitTraining {
 	public static void main(String[] args) {
 		System.out.println("Git training project");
+		System.out.println("Adding some changes");
 	}
 }
