011648b1f662214d158e6488bb1ed32afb74bf8a
Kedar Patil <knpatil512@gmail.com>
2020-02-01 19:39:40 +0530

    Added some changes

--- src/com/training/GitTraining.java
+++ src/com/training/GitTraining.java
@@ -4,5 +4,6 @@ public class GitTraining {
 	public static void main(String[] args) {
 		System.out.println("Git training project");
 		System.out.println("Adding some changes");
+		System.out.println("Master branch changes");
 	}
 }
